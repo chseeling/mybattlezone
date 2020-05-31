15de240408ab81c98246c09c1a2d536b3b61a241
Chris Seeling <cseeling@gmail.com>
2020-05-31 16:01:30 +1000

    update mouse movement

--- test02.py
+++ test02.py
@@ -27,7 +27,7 @@ def procedural_grid(x_min, x_max, y_min, y_max, n):
     x0 = x_min
     x1 = x_max
     y0 = y_min
-    for i in range(0, n+1):
+    for i in range(0, n + 1):
         lines.moveTo(x0, y0, 0.1)
         lines.draw_to(x1, y0, 0.1)
         y0 += del_y
@@ -36,12 +36,52 @@ def procedural_grid(x_min, x_max, y_min, y_max, n):
     y0 = y_min
     y1 = y_max
     x0 = x_min
-    for i in range(0, n+1):
+    for i in range(0, n + 1):
         lines.moveTo(x0, y0, 0.1)
         lines.draw_to(x0, y1, 0.1)
         x0 += del_x
 
     return lines
+
+
+def draw_lines_object(data, idx_start=1):
+    points = data['points']
+    lines_def = data['lines']
+
+    lines = LineSegs()
+    for line_def in lines_def:
+        # print(line_def)
+        idx0 = line_def[0] - idx_start
+        lines.moveTo(points[idx0][0], points[idx0][1], points[idx0][2])
+        for idx1 in line_def[1:]:
+            # print(idx1)
+            idx1 = idx1 - idx_start
+            lines.drawTo(points[idx1][0], points[idx1][1], points[idx1][2])
+    return lines
+
+# n : number of repeats on cylinder
+def map_mountains(points, n):
+    # find range of x
+    xmin = points[0][0]
+    xmax = xmin
+    for point in points[1:]:
+        if xmin > point[0]:
+            xmin = point[0]
+        if xmax < point[0]:
+            xmax = point[0]
+    radius = (xmax - xmin) * n / 2.0 / math.pi
+
+    # map to circle of radius rad
+    points_mapped = []
+    for point in points:
+        z = point[2]
+        theta = (point[0] - xmin) / radius
+        x = math.cos(-theta)
+        y = math.sin(-theta)
+        z = point[2]
+        points_mapped.append([x, y, z])
+    return points_mapped
+
 
 class MyApp(ShowBase):
     def __init__(self):
@@ -52,7 +92,7 @@ class MyApp(ShowBase):
         # base.disableMouse()
         props = WindowProperties()
         # props.setCursorHidden(True)
-        #props.setMouseMode(WindowProperties.M_relative)
+        # props.setMouseMode(WindowProperties.M_relative)
         base.win.requestProperties(props)
         # Load the environment model.
         # self.scene = self.loader.loadModel("models/environment")
@@ -83,14 +123,14 @@ class MyApp(ShowBase):
         self.ground.setScale(50, 50, 1)
         self.mountain.setScale(30, 1, 30)
 
-        # render tanks
-        self.tank.setPos(0, 0, 0)
-        self.tank1 = render.attachNewNode("Tank-Placeholder")
-        self.tank2 = render.attachNewNode("Tank-Placeholder")
-        self.tank1.setPos(100, 100, 0)
-        self.tank2.setPos(150, 70, 0)
-        self.tank.instanceTo(self.tank1)
-        self.tank.instanceTo(self.tank2)
+        # old tanks method
+        # self.tank.setPos(0, 0, 0)
+        # self.tank1 = render.attachNewNode("Tank-Placeholder")
+        # self.tank2 = render.attachNewNode("Tank-Placeholder")
+        # self.tank1.setPos(100, 100, 0)
+        # self.tank2.setPos(150, 70, 0)
+        # self.tank.instanceTo(self.tank1)
+        # self.tank.instanceTo(self.tank2)
 
         # self.cube01.setPos(20, 20, 0)
 
@@ -105,58 +145,47 @@ class MyApp(ShowBase):
             placeholder.setPos(far_rad * sin(angleRadians), far_rad * cos(angleRadians), 0)
             placeholder.setH(placeholder, -i * 10)
             placeholder.setScale(0.4 + 0.6 * random(), 1, 0.1 + 3 * random())
-            #self.mountain.instanceTo(placeholder)
+            # self.mountain.instanceTo(placeholder)
 
+        # new mountain method
         with open('models/digitization01_cleaned_02.json', "r") as f:
             data = json.load(f)
-        points = data['points']
-        lines_def = data['lines']
 
-#   scale mountain_line
-        n = 2   # number of repeats in circumference
-        # find range of x
-        xmin = points[0][0]
-        xmax = xmin
-        for point in points[1:]:
-            if xmin > point[0]:
-                xmin = point[0]
-            if xmax < point[0]:
-                xmax = point[0]
-        radius = (xmax-xmin)*n/2.0/math.pi
-        print(radius)
-        # map to circle of radius rad
-        points_mapped = []
-        for point in points:
-            z = point[2]
-            theta = (point[0] - xmin)/radius
-            x = math.cos(-theta)
-            y = math.sin(-theta)
-            z = point[2]
-            points_mapped.append([x, y, z])
-        points = points_mapped
+        #   map mountain_line to a cylinder
+        n = 2  # number of repeats in circumference
+        data['points'] = map_mountains(data['points'], n)
 
-        lines = LineSegs()
-        for line_def in lines_def:
-            # print(line_def)
-            idx0 = line_def[0] - 1
-            lines.moveTo(points[idx0][0], points[idx0][1], points[idx0][2])
-            for idx1 in line_def[1:]:
-                # print(idx1)
-                idx1 = idx1 - 1
-                lines.drawTo(points[idx1][0], points[idx1][1], points[idx1][2])
-
+        lines = draw_lines_object(data, 1)
         lines.setThickness(3)
         node = lines.create()
         self.np = NodePath(node)
-        scale = 4000
+        scale = 7000
         for i in range(n):
-            angleRadians = 2*pi/n*i
+            angleRadians = 2 * pi / n * i
             placeholder = render.attachNewNode("MountainLine-Placeholder")
-            placeholder.setH(placeholder, 360/n*i)
+            placeholder.setH(placeholder, 360 / n * i)
             # placeholder.setPos(sin(angleRadians), cos(angleRadians), 0)
-            placeholder.setScale(scale, scale, scale/n/2)
+            placeholder.setScale(scale, scale, scale / n / 2.5)
             self.np.instanceTo(placeholder)
-        self.np.setColorScale(0, 1, 0, 1.0)
+        self.np.setColorScale(0, 0.7, 0, 0.5)
+
+        # tank as lines
+        with open('models/tankDesignB.json', "r") as f:
+            data = json.load(f)
+        lines = draw_lines_object(data, 0)
+        lines.setThickness(3)
+        node = lines.create()
+        self.tank = NodePath(node)
+
+        #self.tank.setPos(0, 0, 0)
+        self.tank1 = render.attachNewNode("Tank-Placeholder")
+        self.tank2 = render.attachNewNode("Tank-Placeholder")
+        self.tank1.setPos(100, 100, 0)
+        self.tank2.setPos(150, 70, 0)
+        self.tank.instanceTo(self.tank1)
+        self.tank.instanceTo(self.tank2)
+        self.tank1.setColorScale(0, 0.7, 0, 0.5)
+        self.tank2.setColorScale(1, 0.6, 0.1, 0.5)
 
         # grid
         grid_lines = procedural_grid(-1000, 500, -1000, 500, 50)
@@ -181,20 +210,48 @@ class MyApp(ShowBase):
 
         alight = AmbientLight('ambientLight')
         alight.setColor(Vec4(0, 0, 0, 0))  # ambient light is dim red
-        #alightNP = self.render.attachNewNode(alight)
+        # alightNP = self.render.attachNewNode(alight)
 
         # Add the spinCameraTask procedure to the task manager.
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+        self.taskMgr.add(self.moveTanksTask, "SpinCameraTask")
+
+    def moveTanksTask(self, task):
+        Ax1 = 25;
+        Ay1 = 18
+        Bx1 = -0.1;
+        By1 = 0.2
+        x = Ax1 * sin(Bx1 * task.time) + 10
+        y = Ay1 * sin(By1 * task.time)
+        dx = Ax1 * Bx1 * cos(Bx1 * task.time)
+        dy = Ay1 * By1 * cos(By1 * task.time)
+        heading = math.degrees(math.atan2(dy, dx))
+        self.tank1.setPos(x, y, 0)
+        self.tank1.setH(heading)
+
+        Ax1 = 16;
+        Ay1 = 18
+        Bx1 = 0.25;
+        By1 = 0.3
+        x = Ax1 * sin(Bx1 * task.time) + 20
+        y = Ay1 * sin(By1 * task.time) + 3
+        dx = Ax1 * Bx1 * cos(Bx1 * task.time)
+        dy = Ay1 * By1 * cos(By1 * task.time)
+        heading = math.degrees(math.atan2(dy, dx))
+        self.tank2.setPos(x, y, 0)
+        self.tank2.setH(heading)
+        return Task.cont
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
         angleDegrees = task.time * 10.0
         angleRadians = angleDegrees * (pi / 180.0)
         rad = 200
-        #self.camera.setPos(rad * sin(angleRadians), rad * cos(angleRadians), 4)
+        # self.camera.setPos(rad * sin(angleRadians), rad * cos(angleRadians), 4)
         # self.camera.headsUp(self.tank1, Vec3(0, 0, 1))
         pos = self.camera.getPos()
-        self.camera.setPos(pos[0],pos[1],4)
+        self.camera.setPos(pos[0], pos[1], 5)
         ort = self.camera.getHpr()
         self.camera.setHpr(ort[0], 0, 0)
         # self.camera.setPos(100, 100, 0)
